36da221f3b32dae45433a6a565356a73c5ca464b
miacro <fqguozhou@gmail.com>
2018-04-25 18:18:11 +0800

    ...

--- dockerwrapper/__main__.py
+++ dockerwrapper/__main__.py
@@ -1,15 +1,31 @@
-from pyconfigmanager import get_config
+from pyconfigmanager import get_config, logging_config, helper as config_helper
 import docker
 import os
+import argparse
+import logging
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="docker wrapper")
     schemafile = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), "config", "schema.yaml")
-    config = get_config(schema=schemafile)
-    print(config)
+    config = get_config(schema=[{
+        key: value
+        for key, value in config_helper.get_helpers().items()
+        if key in ("config", "logging")
+    }, schemafile])
+    config.update_values_by_argument_parser(parser=parser)
+    if config.config.dump:
+        config.dump_config(config_name="config.dump", exit=True)
+    logging_config.logger_config(
+        name="dockerwrapper", level=config.logging.verbosity, propagate=False)
+    logging.info([key for key in config])
+    logging.info(config.build.values())
     client = docker.from_env()
-    print(client.containers.list(all=True))
+    return
+    client.images.build(**config.build.values())
 
 
 if __name__ == "__main__":
